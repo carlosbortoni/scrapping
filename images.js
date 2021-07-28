8f8fd4e8060004a713360def573d2d1e56eae7d5
Carlos Bortoni <carlosbortoni@fillet.com.br>
2021-07-28 18:17:58 -0300

    adicionado callback de erro

--- images.js
+++ images.js
@@ -6,6 +6,11 @@ const url = "https://domia.com.br/assets/images/logo-domia.png";
 https.get(url, (res) => {
   const fileStream = fs.createWriteStream('./images/logo.png');
   res.pipe(fileStream);
+
+  fileStream.on('error', (err) => {
+    console.log(err)
+  })
+
   fileStream.on('finish', () => {
     fileStream.close();
     console.log('Download done!');
