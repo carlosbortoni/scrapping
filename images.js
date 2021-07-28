3d4a22e7571af728f003fa5d83de06528d4be179
Carlos Bortoni <carlosbortoni@fillet.com.br>
2021-07-28 18:35:31 -0300

    refatorando

--- images.js
+++ images.js
@@ -1,18 +1,32 @@
 const https = require('https');
 const fs = require('fs');
+const path = require('path');
 
-const url = "https://domia.com.br/assets/images/logo-domia.png";
+downloadFile = (url, callback) => {
+  const filename = path.basename(url);
+  const req = https.get(url, (res) => {
+    const fileStream = fs.createWriteStream(`./images/${filename}`);
+    res.pipe(fileStream);
+  
+    fileStream.on('error', (err) => {
+      console.log(err)
+    })
 
-https.get(url, (res) => {
-  const fileStream = fs.createWriteStream('./images/logo.png');
-  res.pipe(fileStream);
-
-  fileStream.on('error', (err) => {
+    fileStream.on('close', () => {
+      callback(filename)
+    })
+  
+    fileStream.on('finish', () => {
+      fileStream.close();
+      console.log('Download done!');
+    })
+  });
+  
+  req.on('error', (err) => {
     console.log(err)
   })
+}
 
-  fileStream.on('finish', () => {
-    fileStream.close();
-    console.log('Download done!');
-  })
+downloadFile("https://domia.com.br/assets/images/logo-domia.png", (fn) => {
+  console.log(fn);
 });
